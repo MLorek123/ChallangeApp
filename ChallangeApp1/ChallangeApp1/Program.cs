b06771dac6cb6b336562f015bb9ea167949f5cb4
MLorek123 <torlof922@gmail.com>
2024-02-20 17:52:47 +0100

    Dzień 4 zadanie

--- ChallangeApp1/ChallangeApp1/Program.cs
+++ ChallangeApp1/ChallangeApp1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+string Name = "Marcin";
+char Sex = 'M';
+var Age = 26;
+
+if (Sex == 'K' && Age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (Name == "Ewa" && Age == 30)
+{
+    Console.WriteLine("Ewa,lat 30");
+}
+else if (Age < 18 && Sex == 'M')
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
